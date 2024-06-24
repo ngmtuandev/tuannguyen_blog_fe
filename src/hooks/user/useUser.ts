4b1cbe221a63c8cc81d6d537e02ac1fe353057b8
ngmtuandev <nguyenmanhtuancomputer@gmail.com>
2024-06-24 15:49:47 +0700

    fix/error for deloy init

--- src/hooks/user/useUser.ts
+++ src/hooks/user/useUser.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { apiRegister } from "../../service";
 
 import { TRegister } from "../../types";
